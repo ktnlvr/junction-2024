706eb8ae421971c1ff3fc7e470be7bf23ac22fc2
Unai San Segundo <unais@metropolia.fi>
2024-11-09 14:33:04 +0200

    Restructure/clean repo

--- mock.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Product:
-    name: str
-    decay: float
-
-TOMATO = Product("Tomato", 0.5)
-SALAD = Product("Salad")
--- model.py
+++
@@ -1,3 +0,0 @@
-from tinygrad import Tensor
-
-# rnn
